6948c4dec30007291de985e787882103436dda9b
pdscxz <128346269+pdscxz@users.noreply.github.com>
2023-12-26 16:51:45 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://tt.iitvba.com");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://t.tvbox.life");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
