47e8e8de71fa77db47a03196b49e021e7eec14c7
pdscxz <128346269+pdscxz@users.noreply.github.com>
2024-03-06 12:49:21 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://mirror.ghproxy.com/https://raw.githubusercontent.com/pdscxz/TV/main/itvba/tv.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://tt.iitvba.com");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
